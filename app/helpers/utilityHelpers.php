6280463e892f2b9386fe155dbc6864ffcd91fa4a
Monica Wood <monica@medeopolis.au>
2026-03-24 22:33:37 +1100

    Allow ids in purifier. This is important for bookmarking

--- app/helpers/utilityHelpers.php
+++ app/helpers/utilityHelpers.php
@@ -5065,6 +5065,7 @@ function caFileIsIncludable($ps_file) {
 	 */
 	function caGetHTMLPurifier(?array $options=null) : HTMLPurifier {
 		$config = HTMLPurifier_Config::createDefault();
+		$config->set('Attr.EnableID', true);
 		$config->set('URI.DisableExternalResources', !Configuration::load()->get('purify_allow_external_references'));
 		$config->set('Cache.SerializerPath', Configuration::load()->get('purify_serializer_path'));
 		return new HTMLPurifier($config); 
