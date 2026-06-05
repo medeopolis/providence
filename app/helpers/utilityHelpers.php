9a3835b281add0774a9eef4a5b09cee3661a7435
Monica Wood <monica@medeopolis.au>
2026-06-05 04:03:21 +0000

    update the caGetHTMLPurifier to check for dir
    If the directory doesn't exist, create the directory and set the
    owner/group to that of the CA tmp directory

--- app/helpers/utilityHelpers.php
+++ app/helpers/utilityHelpers.php
@@ -5068,6 +5068,15 @@ function caFileIsIncludable($ps_file) {
 		# Swapping out config with html5 compatiable config
 		$config = HTMLPurifier_HTML5Config::createDefault();
 		$config->set('URI.DisableExternalResources', !Configuration::load()->get('purify_allow_external_references'));
+		$serializer_path = Configuration::load()->get('purify_serializer_path');
+		# Check if path exists, if not make dir and set owner:group to same as ca tmp dir
+		if(!file_exists($serializer_path)) { 
+			$tempdir_info = stat(Configuration::load()->get('ca_temp_dir'));
+			mkdir($serializer_path, 0770, true);
+			chown($serializer_path, $tempdir_info['uid']);
+			chgrp($serializer_path, $tempdir_info['gid']);
+			clearstatcache();
+		}
 		$config->set('Cache.SerializerPath', Configuration::load()->get('purify_serializer_path'));
 		return new HTMLPurifier($config); 
 	}
